799e8d6aead1cf19b0c63ddf38457fce9bd6c5cd
Hidetaka Okamoto <info@wp-kyoto.net>
2021-01-14 16:31:30 +0900

    feat !: remove props

--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-
 import './ui.module.css';
 
-/* eslint-disable-next-line */
-export interface UiProps {}
-
-export function Ui(props: UiProps) {
+export function Ui() {
   return (
     <div>
       <h1>Welcome to UI!</h1>
-      <pre><code>{JSON.stringify(props, null, 2)}</code></pre>
     </div>
   );
 }
